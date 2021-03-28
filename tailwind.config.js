fd52a28a0a59e1b8931f69fb1f970c84e8c645b0
Fábio M.R. Barbosa <hello@fabiomrbarbosa.com>
2021-03-29 00:38:29 +0200

    Override styles of prose headings with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,41 @@
 module.exports = {
-  purge: [
-    './src/**/*.njk',
-    './src/**/*.js',
-  ],
+  purge: ["./src/**/*.njk", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'display': ['ClashDisplay', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
-      'serif': ['Gambetta', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif', 'Apple Color Emoji', 'Segoe UI Emoji']
+      display: [
+        "ClashDisplay",
+        "system-ui",
+        "-apple-system",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+      serif: [
+        "Gambetta",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+      ],
     },
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h2: {
+              fontFamily: "'ClashDisplay' system-ui -apple-system Segoe UI Roboto Helvetica Arial sans-serif",
+              fontWeight: '500'
+            },
+          },
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
