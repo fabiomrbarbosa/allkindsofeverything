ba13bcf6c121049de76c4e9bece806eeae841089
Fábio M.R. Barbosa <hello@fabiomrbarbosa.com>
2021-03-28 12:16:38 +0200

    Remove inline CSS transform

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    process.env === 'production' ? require('cssnano')({ preset: 'default' })() : false,
+    process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' })() : false,
   ]
 }
